18c8f55e56fee2ca6ea17bb3c05a886d41892812
HitzeD <d-hitze@comcast.net>
2019-05-18 09:57:01 -0500

    messed with app style, revert for now

--- educell/src/App.js
+++ educell/src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import styled from 'styled-components';
 
-import SideBar from './components/SideBar';
+import SideBar from './view/SideBar';
+import NavBar from './view/NavBar';
+import Login from './components/Login';
+import Home from './view/Home';
 
 import './App.css';
+
+const SideDiv = styled.div`
+  height: 100vh;
+`
 
 
 function App() {
   return (
     <div className="App">
-      <SideBar />
+      <NavBar />
+      <Route exact path='/' component={Home} />
+      <Route path='/login' component={Login} />
+      <div>
+        <SideBar />
+      </div>
     </div>
   );
 }
