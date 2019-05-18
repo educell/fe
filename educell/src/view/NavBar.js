10f0daa33019055199ed65bce15c51b2195d499b
HitzeD <d-hitze@comcast.net>
2019-05-18 09:56:10 -0500

    created navbar added routes

---
+++ educell/src/view/NavBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+const Nav = styled.div`
+    display: flex;
+    height: 20%;
+`
+
+const NavList = styled.ul`
+    list-style-type: none;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+`
+
+const Empty = styled.div`
+    width: 80%;
+`
+
+const NavItem = styled.li`
+    margin: 0 20px;
+`
+
+const NavBar = () => {
+    return (
+        <Nav>
+            <Empty></Empty>
+            <NavList>
+                <NavLink exact to='/'>Home</NavLink> 
+                <NavLink to="/login">Log In</NavLink> {/* NavLinks be here and next line later */}
+                <NavItem>Create an Account</NavItem>
+            </NavList>
+        </Nav>    
+    )
+}
+
+export default NavBar;
