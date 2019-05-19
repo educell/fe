22b24b4e307d12c3162f0a026e50bf96bc0278c1
HitzeD <d-hitze@comcast.net>
2019-05-19 10:43:14 -0500

    added dashboard and private route to app.js

--- educell/src/App.js
+++ educell/src/App.js
@@ -3,9 +3,11 @@ import { Route } from 'react-router-dom';
 import styled from 'styled-components';
 
 import NavBar from './view/NavBar';
-import Login from './components/Login';
+import Login from './components/login pages/Login';
 import Home from './view/Home';
-import NewUser from './components/NewUser';
+import NewUser from './components/login pages/NewUser';
+import DashBoard from './components/DashBoard';
+import PrivateRoute from './components/login pages/PrivateRoute';
 
 import './App.css';
 
@@ -22,6 +24,7 @@ function App() {
       <Route exact path='/' component={Home} />
       <Route path='/login' component={Login} />
       <Route path='/createUser' component={NewUser} />
+      <PrivateRoute exact path='/dashboard' component={DashBoard} />
     </div>
   );
 }
