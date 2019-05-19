34b1d1ac1e767f95a777a3fbcf22d5fefaf642b1
HitzeD <d-hitze@comcast.net>
2019-05-19 09:44:44 -0500

    login says ln 50 is uncontrolled for some reason

--- educell/src/components/Login.js
+++ educell/src/components/Login.js
@@ -1,4 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+
+
+const FormDiv = styled.div`
+    border: 1px solid red;
+    width: 20%;
+    border-radius: 10px;
+    position: relative;
+    left: 700px;
+    top: 300px;
+    padding: 30px;
+
+`
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    justify-content: center;
+    align-items: center;
+    margin: 20px auto;
+`
 
 class Login extends React.Component {
     constructor(){
@@ -12,6 +34,7 @@ class Login extends React.Component {
     }
 
     handleChanges = e => {
+        console.log(e.target.name, e.target.value)
         this.setState({
             credentials: {
                 [e.target.name]: e.target.value,
@@ -21,13 +44,13 @@ class Login extends React.Component {
 
     render(){
         return(
-            <div>
-                <form>
+            <FormDiv>
+                <Form onSubmit={null}>
                     <input onChange={this.handleChanges} name="username" type="text" placeholder="Enter Username" value={this.state.credentials.username} />
                     <input onChange={this.handleChanges} name="password" type="password" placeholder="Enter Password" value={this.state.credentials.password} />
                     <button>Log In</button>
-                </form>
-            </div>
+                </Form>
+            </FormDiv>
         )
     }
 }
