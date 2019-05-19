262b03f4fc8236f8deff70b1742cbeffb3dcd11f
HitzeD <d-hitze@comcast.net>
2019-05-19 10:43:00 -0500

    created dashboard and private route file

---
+++ educell/src/components/DashBoard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class DashBoard extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>Hello Admin/Student/Social Worker</h1>
+            </div>
+        )
+    }
+}
+
+export default DashBoard;
