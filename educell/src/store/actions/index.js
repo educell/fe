9f10788aaf762c2e45ea0d43775d41a1b0094446
HitzeD <d-hitze@comcast.net>
2019-05-21 07:26:28 -0500

    setup more proper action/reducer

--- educell/src/store/actions/index.js
+++ educell/src/store/actions/index.js
@@ -1,1 +1,11 @@
 import axios from 'axios';
+
+export const LOGIN_START = 'LOGIN_START';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+export const login = creds => dispatch => {
+    dispatch({ type: LOGIN_START });
+    return axios.post('https://edu-cell.herokuapp.com/api/auth/login', creds)
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+}
--- educell/src/store/reducers/index.js
+++ educell/src/store/reducers/index.js
@@ -1,15 +1,39 @@
+import { LOGIN_START, LOGIN_SUCCESS, LOGIN_FAILURE } from '../actions';
 const initialState = {
     students: [],
     schools: [],
+    teachers: [],
+    admins: [],
     isLoggedIn: false,
     isLoggingIn: false,
     error: null,
-    
+
 };
 
 const reducer = (state = initialState, action) => {
     switch(action.type){
-
+        case LOGIN_START:
+            return {
+                ...state,
+                isLoggingIn: true,
+                error: '',
+            }
+        case LOGIN_SUCCESS:
+            return {
+                ...state,
+                students: [],
+                isLoggingIn: false,
+                isLoggedIn: true,
+            }
+        case LOGIN_FAILURE:
+            return {
+                ...state,
+                isLoggingIn: false,
+                isLoggedIn: false,
+                error: action.payload,
+            }
+        default:
+            return state;
     }
 };
 
