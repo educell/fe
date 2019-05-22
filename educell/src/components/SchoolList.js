25d35c8595594ea10924071e699da0021c162ac9
HitzeD <d-hitze@comcast.net>
2019-05-22 12:09:33 -0500

    file formatted to receive data

---
+++ educell/src/components/SchoolList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class SchoolList extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                {/* map through schools and display names here */}
+            </div>
+        )
+    }
+}
+
+export default SchoolList;
