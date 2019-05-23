8e1a9f3e7205d6774be35d82d165f7e4e96aa3cd
HitzeD <d-hitze@comcast.net>
2019-05-23 17:10:28 -0500

    full student page containing students and info

---
+++ educell/src/components/Students.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
+
+const Student = styled.div`
+    border: 1px solid gray;
+`
+
+class Students extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Students</h1>
+                <div>
+                    {this.props.students.map(student => {
+                        return(
+                            <Student key={student.id}>
+                                <h1>{student.name}</h1>
+                                <hr/>
+                                <h2>Age: {student.age}</h2>
+                                <h2>Grade: {student.grade}</h2>
+                                <h2>Representative: {student.representative}</h2>
+                                <h2>Emergency Contact: {student.contactInfo}</h2>
+                            </Student>
+                        )
+                    })}
+                </div>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    students: state.students,
+})
+
+export default connect(mapStateToProps, {})(Students);
