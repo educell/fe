4864f9ac3c7518381166a6f0e0cd2869fd85dfea
HitzeD <d-hitze@comcast.net>
2019-05-23 17:12:21 -0500

    removed unneeded imports

--- educell/src/components/DashBoard.js
+++ educell/src/components/DashBoard.js
@@ -1,27 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
 import { withRouter, Route, Switch } from 'react-router-dom';
 
-import NavBar from '../view/NavBar';
 import DashHome from '../view/DashHome';
 import Students from './Students';
-
-const Whole = styled.div`
-    display: flex;
-    height: 100vh;
-`
-
-const Left = styled.div`
-    background-color: #B8D9E5;
-    width: 10%;
-`
-
-const Right = styled.div`
-    width: 90%;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-`
 
 const DashBoard = () => {
     return (
